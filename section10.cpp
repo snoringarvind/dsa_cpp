67b1ef0c1c999718b7f16fdb752b557cccbb8c48
arvind <snoringarvind@gmail.com>
2022-09-01 13:42:15 +0530

    added operator overloading

--- section10.cpp
+++ section10.cpp
@@ -11,125 +11,134 @@ public:
 
 class Sparse
 {
-public:
+private:
     int m;
     int n;
     int numOfElements;
     Element *e;
 
+public:
     Sparse(int m, int n, int numOfElements)
     {
         this->m = m;
         this->n = n;
         this->numOfElements = numOfElements;
     }
-
-    void Read()
+    ~Sparse()
     {
-        e = new Element[numOfElements];
-        cout << "Enter i,j,x values" << endl;
-
-        for (int i = 0; i < numOfElements; i++)
-        {
-            cin >> e[i].i;
-            cin >> e[i].j;
-            cin >> e[i].x;
-            cout << endl;
-        }
-    }
-
-    void Display()
-    {
-        int k = 0;
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (i == e[k].i && j == e[k].j)
-                {
-                    cout << e[k].x;
-                    cout << " ";
-                    k++;
-                }
-                else
-                {
-                    cout << 0;
-                    cout << " ";
-                }
-            }
-            cout << endl;
-        }
-    }
-
-    // instead of copying obejct B in function add, we will pass a pointer to b;
-    Sparse *Add(Sparse *B)
-    {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        if (m != B->m || n != B->n)
-        {
-            cout << "Order of A and B should Be same"
-                 << endl;
-            return 0;
-        }
-
-        struct Sparse *C = (struct Sparse *)malloc(sizeof(struct Sparse));
-        C->m = m;
-        C->n = n;
-        C->e = (struct Element *)malloc((numOfElements + B->numOfElements) * sizeof(struct Element));
-
-        while (i < numOfElements && j < B->numOfElements)
-        {
-            if (e[i].i == B->e[j].i && e[i].j == B->e[j].j)
-            {
-                C->e[k] = e[i++];
-                C->e[k++].x += B->e[j++].x;
-            }
-            else if (e[i].i == B->e[j].i && e[i].j > B->e[j].j)
-                C->e[k++] = B->e[j++];
-            else if (e[i].i == B->e[j].i && e[i].j < B->e[j].j)
-                C->e[k++] = e[i++];
-            else if (e[i].i > B->e[j].i)
-                C->e[k++] = B->e[j++];
-            else if (e[i].i < B->e[j].i)
-                C->e[k++] = e[i++];
-        }
-
-        // COPY REMAINING ELEMENTS
-        for (; i < numOfElements; i++)
-        {
-            C->e[k++] = e[i];
-        }
-
-        for (; j < B->numOfElements; j++)
-        {
-            C->e[k++] = B->e[j];
-        }
-
-        C->numOfElements = k;
-
-        return C;
+        delete[] e;
     };
+
+    friend istream &operator>>(istream &is, Sparse &s);
+    friend ostream &operator<<(ostream &os, Sparse &s);
+    Sparse operator+(Sparse &B);
+};
+
+istream &operator>>(istream &is, Sparse &s)
+{
+    s.e = new Element[s.numOfElements];
+    cout << "Enter i,j,x values" << endl;
+
+    for (int i = 0; i < s.numOfElements; i++)
+    {
+        is >> s.e[i].i;
+        is >> s.e[i].j;
+        is >> s.e[i].x;
+        cout << endl;
+    }
+    return is;
+};
+
+ostream &operator<<(ostream &os, Sparse &s)
+{
+    int k = 0;
+    for (int i = 0; i < s.m; i++)
+    {
+        for (int j = 0; j < s.n; j++)
+        {
+            if (i == s.e[k].i && j == s.e[k].j)
+            {
+                cout << s.e[k].x;
+                cout << " ";
+                k++;
+            }
+            else
+            {
+                cout << 0;
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return os;
+};
+
+Sparse Sparse::operator+(Sparse &B)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    if (m != B.m || n != B.n)
+    {
+        cout << "Order of A and B should Be same"
+             << endl;
+        // return 0;
+    }
+
+    struct Sparse *C = (struct Sparse *)malloc(sizeof(struct Sparse));
+    C->m = m;
+    C->n = n;
+    C->e = (struct Element *)malloc((numOfElements + B.numOfElements) * sizeof(struct Element));
+
+    while (i < numOfElements && j < B.numOfElements)
+    {
+        if (e[i].i == B.e[j].i && e[i].j == B.e[j].j)
+        {
+            C->e[k] = e[i++];
+            C->e[k++].x += B.e[j++].x;
+        }
+        else if (e[i].i == B.e[j].i && e[i].j > B.e[j].j)
+            C->e[k++] = B.e[j++];
+        else if (e[i].i == B.e[j].i && e[i].j < B.e[j].j)
+            C->e[k++] = e[i++];
+        else if (e[i].i > B.e[j].i)
+            C->e[k++] = B.e[j++];
+        else if (e[i].i < B.e[j].i)
+            C->e[k++] = e[i++];
+    }
+
+    // COPY REMAINING ELEMENTS
+    for (; i < numOfElements; i++)
+    {
+        C->e[k++] = e[i];
+    }
+
+    for (; j < B.numOfElements; j++)
+    {
+        C->e[k++] = B.e[j];
+    }
+
+    C->numOfElements = k;
+
+    return *C;
 };
 
 int main()
 {
     Sparse A(5, 6, 5);
-    A.Read();
+    cin >> A;
     cout << "Matrix A==" << endl;
-    A.Display();
+    cout << A;
 
     Sparse B(5, 6, 6);
-    B.Read();
-
+    cin >> B;
     cout << "Matrix B==" << endl;
-    B.Display();
+    cout << B;
 
     cout << "Matrix C==" << endl;
-    Sparse *C = A.Add(&B);
+    Sparse C = A + B;
+    cout << C;
 
-    C->Display();
     return 0;
 }
